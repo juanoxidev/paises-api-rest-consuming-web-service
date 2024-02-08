bb3db04901ff085cfc2765ab0d62d3a20f3354d3
joxinalde <joxinalde@csjn.gov.ar>
2024-02-08 10:29:04 -0300

    first commit

---
+++ src/main/java/com/paisesapirestconsumingwebservice/PaisesApiRestConsumingWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paisesapirestconsumingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisesApiRestConsumingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisesApiRestConsumingWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paisesapirestconsumingwebservice/PaisesApiRestConsumingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paisesapirestconsumingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisesApiRestConsumingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
